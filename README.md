bc51456d42f19cc0fafa10967569bbd147cb5eb8
Matthrews <matthrews@outlook.com>
2022-05-08 21:04:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-rrlxfc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rrlxfc)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent implements OnInit {
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+  items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+  onSubmit() {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+
+    this.checkoutForm.reset();
+  }
+  ngOnInit() {}
+}
